ee211b761dc7a95db704f2b6752d6c6017fa3580
AVS <shrivats@princeton.edu>
2021-06-14 16:51:46 -0400

    Added risk measure calculation functions (VaR, CVaR) to analyze_prescient_output.py. Also improved the plotting methodology to be more flexible and take in any arbitrary list

--- our_scripts/output_analysis/analyze_prescient_output.py
+++ our_scripts/output_analysis/analyze_prescient_output.py
@@ -5,26 +5,70 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 os.chdir("..")
 os.chdir("./collated_outputs")
 
-output_data = pd.read_csv("collated_output_2.csv")
+data = pd.read_csv("collated_output_2.csv")
+baseline = pd.read_csv("collated_output_baseline.csv", index_col=0)
 
 
-def produce_hist(col_name):
+def average_runs(df):
+    return df.mean()
+
+
+def produce_hist(lst, desc, plot_var=True):
     plt.figure()
-    plt.hist(output_data[col_name])
-    plt.xlabel(col_name)
+    plt.hist(lst, bins=math.floor(len(lst)/20))
+    plt.xlabel(desc)
     plt.ylabel("Frequency")
-    plt.title("Histogram of " + col_name + " over Fictitious Scenarios")
+    plt.title("Histogram of " + desc + " over Fictitious Scenarios")
+    if plot_var:
+        plt.axvline(VaR(lst, 0.05, True), color='r')
     plt.show()
 
 
-def produce_scatter(col1, col2):
+def produce_scatter(lst_x, lst_y, desc_x, desc_y):
     plt.figure()
-    plt.scatter(output_data[col1], output_data[col2])
-    plt.xlabel(col1)
-    plt.ylabel(col2)
-    plt.title(col1 + " vs " + col2)
+    plt.scatter(lst_x, lst_y)
+    plt.xlabel(desc_x)
+    plt.ylabel(desc_y)
+    plt.title(desc_x + " vs " + desc_y)
     plt.show()
+
+
+def VaR(lst, alpha, upper_tail = True):
+    # lst: list that we are interested in finding the VaR of
+    # alpha: the level of VaR we are interested in
+    # upper_tail: if true, then we will report the upper tail at alpha. if false, we will report the lower tail
+
+    lst = sorted(lst)
+    ecdf = (np.arange(len(lst)) + 1)/len(lst)
+    if upper_tail:
+        return lst[np.where(ecdf >= 1 - alpha)[0][0]]
+    else:
+        return lst[np.where(ecdf >= alpha)[0][0]]
+
+
+def CVaR(lst, alpha, upper_tail = True):
+    # lst: list that we are interested in finding the VaR of
+    # alpha: the level of VaR we are interested in
+    # upper_tail: if true, then we will report the upper tail at alpha. if false, we will report the lower tail
+
+    lst = sorted(lst)
+    ecdf = (np.arange(len(lst)) + 1)/len(lst)
+    if upper_tail:
+        idx = np.where(ecdf >= 1 - alpha)[0][0]
+        return np.mean(lst[idx:])
+    else:
+        idx = np.where(ecdf >= alpha)[0][0]
+        return np.mean(lst[0:idx+1])
+
+
+baseline_avg = average_runs(baseline)
+print(VaR(data['Total costs'] - baseline_avg['Total costs'], 0.05, True))
+produce_hist(data['Total costs'] - baseline_avg['Total costs'], 'Total costs')
+
+
