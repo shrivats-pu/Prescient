671741012c12641c23cb6524509fc63444845e9e
EthanReese <ethan.reese01@gmail.com>
2021-06-29 10:33:27 -0600

    added runtime infrastructure to dropout assets

---
+++ our_scripts/parallel_versions/parallel_drop.py
@@ -0,0 +1,32 @@
+# parallel_drop.py: Runs dropout tests based on command line argument
+# intended system: Tiger
+# author: Ethan Reese
+# email: ereese@princeton.edu
+# Created: June 29, 2021
+
+import os
+import prescient_helpers.run_helpers as rh
+import sys
+
+
+solar_path = "./solar_quotients.csv"
+no_solar_path = "./no_solar_quotients.csv"
+runs = 1000
+
+dropped_asset = [sys.argv[1]]
+path_template = "./scenario_dropped_" + dropped_asset[0][24:-21]
+
+def run(i):
+        rh.copy_directory(i, path_template)
+        os.chdir(path_template+'%03d'%i)
+        rh.perturb_data(rh.file_paths_combined, solar_path, no_solar_path, dropped_assets=dropped_asset)
+        rh.run_prescient(i, True)
+        os.chdir("..")
+
+# program body
+os.chdir("..")
+os.chdir("..")
+os.chdir("./downloads")
+
+for j in range(runs):
+        run(j)
